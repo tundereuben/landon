e5b3faa8a01a7dfb8e57988b0e40d75215e1beca
Tunde Ogunjimi <tundereuben@yahoo.com>
2023-06-22 17:52:07 +0100

    Added JSON to represent data and make components dynamic

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Welcome from './Welcome'
+import HotelInfo from './HotelInfo'
+
+function Main() {
+  return (
+    <main id="wrapper">
+        <Welcome />
+        <HotelInfo />
+    </main>
+  )
+}
+
+export default Main
